37e92a237a55ee71cbf41d74dde0ef46fa6ab45b
Chaser <florian.isak@icloud.com>
2024-09-13 10:14:20 +0200

    Add files for website_part

---
+++ website_part/script.js
@@ -0,0 +1,69 @@
+const BASE_URL = "https://api.thecatapi.com/v1";
+
+const randomCatsContainer = document.querySelector('#random-cats-container');
+const favoritesCatsContainer = document.querySelector('#favorites-cats-container');
+
+
+const getRandomCats = async() => {
+    const response = await fetch(`${BASE_URL}/images/search?limit=10`)
+    const data = await response.json();
+    displayCats(data, randomCatsContainer, false)
+}
+
+const displayCats = (cats, container, isFavorite) =>{
+    container.innerHTML = '';
+    cats.forEach(cat=>{
+        const catDiv = document.createElement("div");
+        catDiv.classList.add("cat-item");
+
+        const img = document.createElement("img")
+        img.classList.add("cat-image");
+        img.src= cat.url;
+
+        const favButton = document.createElement("button");
+        favButton.classList.add("fav-button");
+        favButton.innerText = isFavorite ? "Unlike" : "Like";
+        favButton.addEventListener("click", ()=>{
+            if(isFavorite){
+                removeFavorites(cat.id);
+            }else{
+                addFavorites(cat)
+            }
+        });
+        catDiv.appendChild(img);
+        catDiv.appendChild(favButton);
+        container.appendChild(catDiv);
+    })
+}
+
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+const addFavorites = (cat) =>{
+    if(!favorites.some(fav => fav.id === cat.id)){
+        favorites.push(cat);
+        localStorage.setItem("favorites", JSON.stringify(favorites))
+        displayFavorites();
+    }
+}
+
+const removeFavorites = (catId) => {
+    favorites = favorites.filter(cat => cat.id !== catId);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    displayFavorites();
+};
+
+const displayFavorites = () => {
+    favoritesCatsContainer.innerHTML = '';
+    if (favorites.length === 0) {
+        const emptyMessage = document.createElement('p');
+        emptyMessage.classList.add('empty');
+        emptyMessage.innerText = "Aucun chat en favoris";
+        favoritesCatsContainer.appendChild(emptyMessage);
+    } else {
+        displayCats(favorites, favoritesCatsContainer, true);
+    }
+};
+
+
+getRandomCats();
+displayFavorites();
